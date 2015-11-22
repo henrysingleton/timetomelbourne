642897525af052f4972bdf2fbf4f4005b37bdcc7
Henry Singleton <henry.singleton@squareweave.com.au>
2015-11-22 16:32:13 +1100

    Nicer handling of location coords in admin

--- pttime/admin.py
+++ pttime/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from leaflet.admin import LeafletGeoAdmin
-from .models import Location
+from .models import Location, Route
+import json
 
-admin.site.register(Location, LeafletGeoAdmin)
+
+class LocationAdmin(LeafletGeoAdmin):
+    list_display = (['coordinates','suburb','postcode','landmark'])
+    fields = ['postcode', 'suburb', 'landmark', 'geolocation', 'raw_geo_data']
+    readonly_fields = ['raw_geo_data']
+
+    def raw_geo_data(self, obj):
+        return obj.geolocation
+
+
+admin.site.register(Route)
+admin.site.register(Location, LocationAdmin)
--- pttime/models.py
+++ pttime/models.py
@@ -1,29 +1,20 @@
 from django.db import models
 from djgeojson.fields import PointField
-# import json
 
 
-# Create your models here.
 class Location(models.Model):
     postcode = models.CharField(blank=True, max_length=5)
     suburb = models.CharField(max_length=200, blank=True)
     landmark = models.CharField(max_length=200, blank=True)
     geolocation = PointField(blank=True)
 
-    # def do_bulk_create(points):
-    #
-    #     # convert json to objects or something
-    #
-    #
-    #     points = json.loads(points)
-    #
-    #     location_objs = [
-    #         Location(
-    #             location = point.whatever
-    #         )
-    #         for point in points
-    #     ]
-    #     msg = Location.objects.bulk_create(location_objs)
+    class Meta:
+        verbose_name = "Location Point"
+
+    def _get_coordinates(self):
+        return '%s %s' % (self.geolocation['coordinates'][0], self.geolocation['coordinates'][1])
+    coordinates = property(_get_coordinates)
+
 
 class Route(models.Model):
     origin = models.ForeignKey(Location, related_name='origin')
