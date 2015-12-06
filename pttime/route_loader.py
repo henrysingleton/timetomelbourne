82bdf7d775af1d45160821d0f8ffd3a14afcb8c1
Henry Singleton <henry.singleton@squareweave.com.au>
2015-12-07 07:57:46 +1100

    Basic parsing of bing JSON data.

--- pttime/route_loader.py
+++ pttime/route_loader.py
@@ -1,5 +1,8 @@
 from .models import Location, Route
 from . import bingmaps
+from datetime import timedelta
+import json
+from time import sleep
 
 
 class RouteLoader:
@@ -8,7 +11,7 @@ class RouteLoader:
         """Find all locations that will need an update and return them. IE those that have no routes against them,
         or that only have routes against them that are stale"""
 
-        results = Location.objects.raw('''SELECT pttime_location.* FROM pttime_location LEFT JOIN pttime_route ON pttime_route.origin_id = pttime_location.id WHERE pttime_route.id IS NULL ORDER BY pttime_location.id LIMIT 3''')
+        results = Location.objects.raw('''SELECT pttime_location.* FROM pttime_location LEFT JOIN pttime_route ON pttime_route.origin_id = pttime_location.id WHERE pttime_route.id IS NULL ORDER BY pttime_location.id LIMIT 100''')
 
         return results
 
@@ -17,15 +20,38 @@ class RouteLoader:
 
         bingthing = bingmaps.BingMaps(key='Ag3rJe-YpHuknwoKRjZooOoTldyyukufpqLQuyu8VfdXnuqRC7SI30sWMoLtG6bh')
 
-
-
         for location in locations:
             waypoints = [location.coordinates, destination.coordinates]
             print('Processing waypoint %s for id %s' % (waypoints, location.id))
             results = bingthing.get_routes(waypoints, travel_mode='Transit', date_time='9:00:00AM', time_type='Arrival')
+            parsed_results = self.parse_bing_data(results)
 
-            route = Route(origin=location, destination=destination, transfers=0, time='00:00', walking_time='00:00', pain=0, xml_response=results)
+            route = Route(origin=location,
+                          destination=destination,
+                          transfers=parsed_results['transfers'],
+                          time=parsed_results['time'],
+                          walking_time=parsed_results['walking_time'],
+                          response=json.dumps(results))  # Convert back to JSON for storage.
             route.save()
 
             print('Saved route with id %s' % route.id)
+            sleep(4)
 
+    @staticmethod
+    def parse_bing_data(data):
+        """
+
+        :param data:
+        :return: dict
+        """
+        ret = dict()
+        ret['transfers'] = 0
+        ret['time'] = timedelta(seconds=data['travelDuration'])
+        ret['walking_time'] = '00:00'
+
+        return ret
+
+        # results['routeLegs'][0]['itineraryItems']  [x]['travelDuration'] ... [x]['iconType'] == Walk
+        # OR as XPath sum(/Response/ResourceSets/ResourceSet/Resources/Route/RouteLeg/ItineraryItem[Instruction/@maneuve
+        # rType="Walk"]/TravelDuration)
+        # <Hint hintType="TimeToMakeConnection"
